c4b558fea6cde1655b11fb91f94e6952bea5144c
ig <gaibullaeviz@gmail.com>
2025-03-31 18:29:42 +0300

    refactoring: token refresh

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -4,7 +4,11 @@ export const Api = axios.create({
     baseURL: import.meta.env.VITE_SERVER_URL,
     headers: {
         "Content-Type": "application/json",
-        application: "x-www-form-urlencoded",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`
+        application: "x-www-form-urlencoded"
     },
+});
+
+Api.interceptors.request.use((config) => {
+    config.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`
+    return config;
 });
